6cf647d19e6d15ead68b4a49eee9f06c1b387055
Nick <nick@polyforest.com>
2018-04-21 17:23:24 -0500

    hunt view

--- Shell/core/src/ld41/HuntView.kt
+++ Shell/core/src/ld41/HuntView.kt
@@ -20,6 +20,7 @@ import com.acornui.async.launch
 import com.acornui.async.then
 import com.acornui.component.ElementContainerImpl
 import com.acornui.component.UiComponent
+import com.acornui.component.container
 import com.acornui.component.image
 import com.acornui.component.layout.algorithm.CanvasLayoutContainer
 import com.acornui.core.assets.AssetType
@@ -30,6 +31,7 @@ import com.acornui.core.cursor.cursor
 import com.acornui.core.di.Owned
 import com.acornui.core.di.inject
 import com.acornui.core.di.own
+import com.acornui.core.graphics.AtlasComponent
 import com.acornui.core.graphics.atlas
 import com.acornui.core.graphics.perspectiveCamera
 import com.acornui.core.immutable.DataBinding
@@ -37,12 +39,9 @@ import com.acornui.core.input.interaction.click
 import com.acornui.core.mvc.Command
 import com.acornui.core.mvc.CommandType
 import com.acornui.core.time.onTick
-import com.acornui.gl.core.DEFAULT_SHADER_HEADER
 import com.acornui.gl.core.Gl20
-import com.acornui.gl.core.ShaderProgramBase
 import com.acornui.graphics.Color
 import com.acornui.graphics.LightingRenderer
-import com.acornui.graphics.UNPACK_FLOAT
 import com.acornui.graphics.lighting.PointLight
 import com.acornui.graphics.lighting.ambientLight
 import com.acornui.graphics.lighting.directionalLight
@@ -55,12 +54,18 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 
 	val dataBind = DataBinding<TargetVo>()
 
-	private val world: ElementContainerImpl<UiComponent>
+	private val world: World
 
 	private val snipeCam = perspectiveCamera(autoCenter = false) {
-		fieldOfView = 32f * MathUtils.degRad
-		far = 5000f
-		setPosition(0f, -1000f, -4000f)
+		fieldOfView = 16f * MathUtils.degRad
+		setPosition(0f, -1000f, -16000f)
+		far = -position.z + 2000f
+	}
+
+	private val viewCam = perspectiveCamera(autoCenter = false) {
+		fieldOfView = 67f * MathUtils.degRad
+		setPosition(0f, -1000f, -16000f)
+		far = -position.z + 2000f
 	}
 
 	private val windowResizedHandler = { newWidth: Float, newHeight: Float, isUserInteraction: Boolean ->
@@ -74,9 +79,15 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 	private val tmpVec = Vector2()
 
 	private var gunShot: SoundFactory? = null
+	private var chamber: SoundFactory? = null
+
+	private val bullet = Bullet()
+
+	private var shotRequested: Boolean = false
 
 	init {
 		assets.load("assets/sounds/sniperRifle.mp3", AssetType.SOUND).then { gunShot = it }
+		assets.load("assets/sounds/chamber.mp3", AssetType.SOUND).then { chamber = it }
 
 		focusEnabled = true
 
@@ -84,7 +95,6 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 		windowResizedHandler(window.width, window.height, false)
 
 		cursor(StandardCursors.NONE)
-
 
 //		+text {
 //			dataBind.bind {
@@ -109,18 +119,18 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 //			}
 //		}
 
-
 		world = +World(this, camLookAt).apply {
 			cameraOverride = snipeCam
 		} layout { center() }
+
+
 
 		+image {
 			scope = +atlas("assets/hunt.json", "Scope")
 		} layout { center(); fill() }
 
 		click().add {
-			world.testHit()
-			gunShot?.createInstance()?.start()
+			shotRequested = true
 		}
 
 		onTick {
@@ -138,8 +148,42 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 				snipeCam.setUp(Vector3.NEG_Y)
 			}
 
+			if (shotRequested) {
+				shotRequested = false
+				if (!bullet.isActive) {
+					bullet.isActive = true
+					resetBullet(bullet)
+					world.bulletView.visible = true
+					gunShot?.createInstance()?.start()
+				}
+			}
+
+			if (bullet.isActive) {
+				val previousP = bullet.position.z
+				bullet.update()
+				if (bullet.position.z > 0f && previousP < 0f) println("hit")
+				//println("bullet.position ${bullet.position}")
+				world.bulletView.setPosition(bullet.position)
+				if (bullet.life == 40) {
+					chamber?.createInstance()?.start()
+				}
+				if (bullet.life < 0) {
+					bullet.isActive = false
+				}
+			}
+
 		}
 
+		resetBullet(bullet)
+
+	}
+
+	private fun resetBullet(bullet: Bullet) {
+		world.bulletView.visible = false
+
+		bullet.life = 150
+		bullet.position.set(snipeCam.position)
+		bullet.velocity.set(camLookAt).sub(bullet.position).nor().scl(200f)
 	}
 
 	override fun dispose() {
@@ -152,6 +196,7 @@ class HuntView(owned: Owned) : CanvasLayoutContainer(owned) {
 		private const val SCOPE_SIZE_INV = 1f / SCOPE_SIZE
 		private const val SCOPE_MOVE_SPEED = 90f
 		private const val DEAD_ZONE = 0.01f
+
 		private val camMoveWindow = MinMax(-2500f, 2500f, -2000f, 800f)
 	}
 
@@ -173,7 +218,10 @@ class MissCommand(val target: TargetVo) : Command {
 }
 
 
-private class World(owner: Owned, private val camLookAt: Vector2Ro) : ElementContainerImpl<UiComponent>(owner) {
+private class World(
+		owner: Owned,
+		private val camLookAt: Vector2Ro
+) : ElementContainerImpl<UiComponent>(owner) {
 	private var skeletonC: SkeletonComponent? = null
 
 	private val scene: SpineScene
@@ -193,11 +241,23 @@ private class World(owner: Owned, private val camLookAt: Vector2Ro) : ElementCon
 		PointLight()
 	}).toList()
 
+	val bulletView: UiComponent
+
 	init {
 		+repeatTexture("assets/grass.jpg") {
-			setSize(2000f, 2000f)
-			setPosition(-1000f, 0f, -1000f)
+			setSize(8000f, 20000f)
+			setPosition(-4000f, 0f, -16000f)
 			rotationX = PI * 0.5f
+		}
+
+		+container {
+			+BuildingView(this).apply {
+				moveTo(0f, 0f, 1000f)
+			}
+
+			+BuildingView(this).apply {
+				moveTo(BuildingView.WIDTH + 100f, 0f, 1000f)
+			}
 		}
 
 		scene = +spineScene {
@@ -208,6 +268,10 @@ private class World(owner: Owned, private val camLookAt: Vector2Ro) : ElementCon
 					animationState.setAnimation(0, "walk", loop = true)
 				}
 			}
+		}
+
+		bulletView = +atlas("assets/hunt.json", "Bullet") {
+			visible = false
 		}
 	}
 
@@ -229,21 +293,60 @@ private class World(owner: Owned, private val camLookAt: Vector2Ro) : ElementCon
 	private val gl = inject(Gl20)
 
 	override fun draw() {
-//		lightingRenderer.directionalLightCamera.setClipSpaceFromWorld(camLookAt.x - 1000f, camLookAt.y + 1000f, camLookAt.y - 1000f, camLookAt.y + 1000f, -100f, 500f, camera)
+		lightingRenderer.directionalLightCamera.setClipSpaceFromWorld(camLookAt.x - 1200f, camLookAt.y + 1200f, camLookAt.y - 1200f, camLookAt.y + 1200f, -100f, 2000f, camera)
 		lightingRenderer.render(camera, ambientLight, directionalLight, pointLights, {
-			//gl.cullFace(Gl20.FRONT)
 			scene.render()
 		}, {
-			//gl.cullFace(Gl20.BACK)
 			_drawScene()
 		})
-//		super.draw()
 	}
 
 	private fun _drawScene() {
-//		gl.enable(Gl20.DEPTH_TEST)
-//		gl.depthFunc(Gl20.LESS)
 		super.draw()
-//		gl.disable(Gl20.DEPTH_TEST)
 	}
 }
+
+private class Bullet {
+
+	var isActive = false
+
+	val position = Vector3()
+	val velocity = Vector3()
+	val acceleration = GRAVITY
+	var life: Int = 0
+
+	fun update() {
+		position.add(velocity)
+		velocity.add(acceleration)
+		life--
+	}
+
+}
+
+private class BuildingView(owner: Owned) : ElementContainerImpl<UiComponent>(owner) {
+
+	init {
+//		+atlas("assets/hunt.json", "Building") {
+//			setScaling(10f, 10f)
+//			rotationY = -PI / 2f
+//			setPosition(WIDTH, -HEIGHT)
+//		}
+		+atlas("assets/hunt.json", "Building") {
+			setScaling(10f, 10f)
+			setPosition(0f, -HEIGHT)
+		}
+	}
+
+	companion object {
+		const val HEIGHT = 3000f
+		const val WIDTH = 1500f
+	}
+
+}
+
+interface HitTarget {
+
+	fun hitCheck(x: Float, y: Float): Boolean
+}
+
+private val GRAVITY = Vector3(0f, 0.2f, 0f)
