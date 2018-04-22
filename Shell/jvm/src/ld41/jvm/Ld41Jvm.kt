de6a55dd8360dad2976954172f06379d86aa6f38
Nick <nick@polyforest.com>
2018-04-22 16:52:05 -0500

    removed restartJvm

--- Shell/jvm/src/ld41/jvm/Ld41Jvm.kt
+++ Shell/jvm/src/ld41/jvm/Ld41Jvm.kt
@@ -5,11 +5,9 @@ import com.acornui.core.AppConfig
 import com.acornui.core.GlConfig
 import com.acornui.core.WindowConfig
 import com.acornui.jvm.LwjglApplication
-import com.acornui.jvm.restartJvm
 import ld41.Ld41
 
 fun main(args: Array<String>) {
-	if (restartJvm()) return
 	LwjglApplication().start(AppConfig(frameRate = 60, window = WindowConfig(title = "Ld41"), gl = GlConfig(antialias = true))) {
 		stage.addElement(Ld41(this))
 	}
